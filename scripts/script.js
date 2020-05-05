7f696de0c92a887ee3b40f3b74cb64764b68985e
lukaszscislowski <lukas.scislowski@gmail.com>
2020-05-05 19:56:44 +0200

    js function add

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const hamburger = document.querySelector('.navbar__hamburger');
+const navLinks = document.querySelector('.navbar__list');
+
+let clicked = false;
+
+hamburger.addEventListener('click', (e) => {
+  clicked = !clicked;
+  e.preventDefault();
+  e.stopPropagation();
+  hamburger.childNodes[1].src = 'images/icon-close.svg';
+  navLinks.style.display = 'flex';
+
+  if (!clicked) {
+    hamburger.childNodes[1].src = 'images/icon-hamburger.svg';
+    navLinks.style.display = 'none';
+  }
+});
